2741fd294dad8383ada2e905fd1fdc32cd30923a
Nicola Melloni <nicola.melloni@mia-care.io>
2022-10-25 21:00:58 +0200

    removed unused vairable from docker_run

--- docker_run.sh
+++ docker_run.sh
@@ -3,7 +3,6 @@
 set -e
 
 CONTAINER_IMAGE="pharma-geolocation-example"
-CONTAINER_REGISTRY="registry.digitalocean.com/pharma-geolocation-example"
 
 if [ -z "$REACT_APP_GOOGLE_PLACES_API_KEY" ]; then
    echo "Google API key not set. Run 'export REACT_APP_GOOGLE_PLACES_API_KEY=<your-key>' to  have your containerized application properly work."
@@ -12,6 +11,5 @@ fi
 
 yarn install && yarn build
 docker buildx build --platform linux/amd64 -t $CONTAINER_IMAGE .
-docker tag ${CONTAINER_NAME} "${CONTAINER_REGISTRY}/${CONTAINER_IMAGE}"
 docker run -p 80:80 ${CONTAINER_IMAGE}
 
