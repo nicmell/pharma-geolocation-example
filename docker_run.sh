275768c3884cb9d3a2d3a8ac03636ba2b04767bf
Nicola Melloni <nicola.melloni@mia-care.io>
2022-10-25 20:39:41 +0200

    docker up

---
+++ docker_run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+CONTAINER_IMAGE="pharma-geolocation-example"
+CONTAINER_REGISTRY="registry.digitalocean.com/pharma-geolocation-example"
+
+if [ -z "$REACT_APP_GOOGLE_PLACES_API_KEY" ]; then
+   echo "Google API key not set. Run 'export REACT_APP_GOOGLE_PLACES_API_KEY=<your-key>' to  have your containerized application properly work."
+   exit 0
+fi
+
+yarn install && yarn build
+docker buildx build --platform linux/amd64 -t $CONTAINER_IMAGE .
+docker tag ${CONTAINER_NAME} "${CONTAINER_REGISTRY}/${CONTAINER_IMAGE}"
+docker run -p 80:80 ${CONTAINER_IMAGE}
+
